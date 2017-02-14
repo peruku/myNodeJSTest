22f39f5331ace784e08cec775584c0c17ab91e78
peruku <perka.prasad@gmail.com>
2017-02-14 08:44:47 +0800

    existing nodejs poject

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { EmailComponent } from './email/email.component';
+import { SignupComponent } from './signup/signup.component';
+import { MembersComponent } from './members/members.component';
+import { AuthGuard } from './auth.service';
+import { routes } from './app.routes';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyDOvArG45o-OeCycvI4Au9LkOlzksJPo7A",
+    authDomain: "dal2u-8098e.firebaseapp.com",
+    databaseURL: "https://dal2u-8098e.firebaseio.com",
+    storageBucket: "dal2u-8098e.appspot.com",
+    messagingSenderId: "199650731030"
+  };
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    EmailComponent,
+    SignupComponent,
+    MembersComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    routes
+  ],
+  providers: [AuthGuard],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
