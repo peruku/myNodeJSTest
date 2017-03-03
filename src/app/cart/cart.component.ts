044485fc9fd1ab017ba719d88f848919a2993f38
peruku <perka.prasad@gmail.com>
2017-03-03 21:11:11 +0800

    totalCartPrice fixed in cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -3,8 +3,10 @@ import { AngularFire, AuthProviders, AuthMethods, FirebaseListObservable, Fireba
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs'
 import "rxjs/add/operator/map";
+import "rxjs/add/operator/scan";
 import "rxjs/add/operator/filter";
 import { MycartService } from '../mycart.service';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-cart',
@@ -14,15 +16,26 @@ import { MycartService } from '../mycart.service';
 export class CartComponent implements OnInit {
 
   myCartList: Observable<any>;
+  userCartList: Observable<any>;
+  cartHomeList: Observable<any>;
   totalPrice: number = 0;
+  totalPriceAtCart: number;
   authId: any;
+  expTotal: Observable<any>;
+  exp2Total: Observable<any>;
 
-  constructor(public af: AngularFire, private _mycartService: MycartService) {
+  constructor(public af: AngularFire, private _mycartService: MycartService, private _userService: UserService) {
 
     this.af.auth.subscribe(auth => {
       if(auth) {
         this.authId = auth.auth.uid;
-        this.myCartList = af.database.list(`/userInfo/${this.authId}/userCart`)
+        this.myCartList = af.database.list(`/userInfo/${this.authId}/userCart`,{
+                 query: {
+                   orderByChild: 'status',
+                   equalTo: 1
+                 }
+            });
+/*
          .map(items => { return items.filter(item =>{
           if((<any>item).status === 1){
             
@@ -32,12 +45,13 @@ export class CartComponent implements OnInit {
             console.log((<any>item).info.name + " - " +this.totalPrice);
             return item;
           }
-         } ); });
+         } ); });  */
 
 
 
          this.myCartList.subscribe(val =>{
-            console.log(val);
+           // console.log(val);
+
             //val.do(obj => {this.totalPrice += obj.quantity * obj.price;});
             //this.totalPrice = 10;
           });
@@ -57,6 +71,41 @@ export class CartComponent implements OnInit {
    }
 
   ngOnInit() {
+    this._userService.getUser().subscribe(user =>{
+       //this.user = user;
+       //this.userCartList = this._userService.getUserCartInfo(user.auth.uid);
+       if(user){
+          console.log("UID "+user.auth.uid);
+           this._mycartService.getUserCartList(user.auth.uid).subscribe(list =>{
+             this.userCartList = list;
+             console.log(list);
+           });
+           this.cartHomeList = this._mycartService.getUserCartHomeList(user.auth.uid);
+           this.cartHomeList.subscribe(list =>{
+             this.totalPriceAtCart = this._mycartService.getTotalCartPrice();
+           });
+          // this.expTotal = this._mycartService.getUserCartList(user.auth.uid).scan((acc,val) => {
+            //   acc += val.quantity;
+              // return acc;
+           //  });
+           //this.expTotal.subscribe(snapshot => console.log);
+           
+           this.exp2Total = this._mycartService.getUserCartList(user.auth.uid)
+             .map(items => {
+               let total = 0;
+               items.map(item => {
+                 //console.log(item);
+                 total += item.quantity * item.priceAtCart;
+                 //console.log("Full"+total);
+               })
+               return total;
+              });
+             
+           this.exp2Total.subscribe(console.log);
+          
+       }
+       
+    });
   }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -33,104 +33,14 @@ export class HomeComponent implements OnInit {
   myUserInfo: Observable<any>;
   userCartList: Observable<any>;
 
-  constructor(public af: AngularFire,private router: Router, private _userService: UserService, private _mycartService: MycartService) {
-
-     console.log("userLoggedInInfo :"+this._userService.isLoggedIn());
-    //userQuantity need to be added for each item in cartList
-    this.cartList = af.database.list('cartList')
-      .map(items => { return items.filter(item => (<any>item).enable === true); });
-    //this.cartList.subscribe(val => console.log(val));
-    this.af.auth.subscribe(auth => {
-      if(auth) {
-        this.name = auth;
-        this.uid = auth.auth.uid;
-        //console.log(auth);
-        if(auth.auth.displayName){ 
-          this.displayName = this.name.auth.displayName;
-          console.log("email :"+auth.auth.email);
-          console.log("photoURL :"+auth.auth.photoURL);
-          //update userInfo
-          this.af.database.object(`/userInfo/${auth.auth.uid}`).update({
-                name: auth.auth.displayName,
-                email: auth.auth.email
-            });
-        }
-        //this.myCartList = this.af.database.list(`/userInfo/${auth.auth.uid}/userCart`);
-       // this.myCartList.subscribe(value => console.log);
-        this.myCartList = af.database.list(`/userInfo/${auth.auth.uid}/userCart`,{
-           query: {
-             orderByChild: 'status',
-             equalTo: 1
-           }
-         });
-         
-         this.myCartList.subscribe(val =>{
-            //console.log(val);
-            //val.do(obj => {this.totalPrice += obj.quantity * obj.price;});
-            //this.totalPrice = 10;
-          });
-        //console.log(this.myCartList);
-        //this.expCartHomeList = 
-        /*
-         .map(items => { return items.filter(item =>{
-          if((<any>item).status === 1){
-              console.log("cart ID"+(<any>item).$key);
-              let userCartKey = (<any>item).$key;
-              let userCartItemQ = (<any>item).quantity;
-              this.cartList = this.cartList.map(citems => { return citems.filter(citem => {
-                console.log(citem);
-                // Add userQuantity property here
-                if((<any>citem).ID === userCartKey){
-                   (<any>citem).push({userQuantity: userCartItemQ});
-                }
-                return citem;
-              });});
- 
-          }
-         } ); }); */
-        
-      }
-      else {
-        console.log("Anonymus User");
-      }
-    });
-
-    this.myCartList = this.af.database.list(`/userInfo/${this.uid}/userCart`);//.subscribe(list => console.log(list));
-    
-
-  }
-
-  addToCart(id, count){
-    this.isClicked = !this.isClicked;
-    console.log(this.isClicked);
-    let cartObj = {
-                status: 1,
-                quantity: count,
-                at: firebase.database.ServerValue.TIMESTAMP
-            };
-    if(this.name){
-      this.af.database.object(`/userInfo/${this.name.auth.uid}/userCart/${id}`).update(cartObj).then(val =>{
-          this.totalPriceAtCart = this._mycartService.getTotalCartPrice();
-      });
-    }
-    else console.log("User not logged IN");
-    
-  }
-
-  getUserQuantity(key): number{
-    //is not user return zero
-    if(this.user ){
-      console.log(key);
-      console.log(this.userCartList);
-      return 1;
-    } 
-    else return 0;
-  }
+  constructor(public af: AngularFire,
+              private router: Router,
+              private _userService: UserService,
+              private _mycartService: MycartService) {
   
-  clickShipid(id,name,price, count){
-    console.log("id : "+id+" price : "+price+" count : "+count);
-    this.isClicked = !this.isClicked;
-    console.log(this.isClicked);
+  }
+
+  addToCart(id,name,price, count){
     let cartObj = {
                 status: 1,
                 name: name,
@@ -139,71 +49,67 @@ export class HomeComponent implements OnInit {
                 at: firebase.database.ServerValue.TIMESTAMP
             };
     if(this.name){
-      this.af.database.object(`/userInfo/${this.name.auth.uid}/userCart/${id}`).update(cartObj);
+      if(1){//count){
+        console.log("Adding"+name+" to Cart "+count+" itemS");
+        this.af.database.object(`/userInfo/${this.name.auth.uid}/userCart/${id}`).update(cartObj).then(val =>{
+            this.totalPriceAtCart = 0;
+            this.totalPriceAtCart = this._mycartService.getTotalCartPrice();
+        });
+      }
+      else{
+        console.log("Removing"+name+" from Cart "+count+" itemS");
+          this.af.database.object(`/userInfo/${this.name.auth.uid}/userCart/${id}`).remove().then(val =>{
+              this.totalPriceAtCart = 0;
+              //set itemCartHome userQuantity to zeo
+              this.totalPriceAtCart = this._mycartService.getTotalCartPrice();
+          });
+      } 
     }
     else console.log("User not logged IN");
-    /*this.cartList.map(items => {return items.filter(item => (<any>item).ID == id)}).subscribe((firstItem) =>{
-      console.log(firstItem[0].ID);
-      if(this.name){
-        this.myCart = this.af.database.object(`/userInfo/${this.name.auth.uid}/userCart/${firstItem[0].ID}`, {preserveSnapshot: true});
-        this.myCart.subscribe(snapshot => {
-          console.log(snapshot);
-          if(snapshot.exists()){
-            console.log("Already Added");
-            
-            console.log(snapshot.quantity);
-            this.myCartCount = snapshot.quantity;
-          }
-          else{ 
-            console.log("going to add");
-            this.af.database.object(`/userInfo/${this.name.auth.uid}/userCart/${firstItem[0].ID}`).update({
-                status: 1,
-                priceAtCart: firstItem[0].price,
-                quantity: 1,
-                at: firebase.database.ServerValue.TIMESTAMP
-            });
-          }
-        });
-        if(this.myCartCount){
-          console.log("count : "+this.myCartCount);
-          this.af.database.object(`/userInfo/${this.name.auth.uid}/userCart/${firstItem[0].ID}`).update({
-                status: 1,
-                priceAtCart: firstItem[0].price,
-                quantity: this.myCartCount+1,
-                at: firebase.database.ServerValue.TIMESTAMP
-            });
-        }
-        else{
-          console.log("First Add");
-           this.af.database.object(`/userInfo/${this.name.auth.uid}/userCart/${firstItem[0].ID}`).update({
-                status: 1,
-                priceAtCart: firstItem[0].price,
-                quantity: 1,
-                at: firebase.database.ServerValue.TIMESTAMP
-            });
-        }
-      }
-      else console.log("Error :Need to Login First !");
-    }); */
+    
   }
-
+  
   logout() {
      this.af.auth.logout();
      console.log('logged out');
+     this.displayName = 'Anonymus';
      //this.router.navigateByUrl('/login');
   }
 
   ngOnInit() {
     this._userService.getUser().subscribe(user =>{
-       this.user = user;
-       this.userCartList = this._userService.getUserCartInfo(user.auth.uid);
-       this.cartHomeList = this._mycartService.getUserCartHomeList(user.auth.uid);
-       this.cartHomeList.subscribe(list =>{
-         this.totalPriceAtCart = this._mycartService.getTotalCartPrice();
-       });
+      if(user){
+        console.log("User Now Logged IN");
+               this.name = user;
+               this.uid = user.auth.uid;
+                //console.log(auth);
+                if(user.auth.displayName){ 
+                  this.displayName = this.name.auth.displayName;
+                  console.log("email :"+user.auth.email);
+                  console.log("photoURL :"+user.auth.photoURL);
+                  //update userInfo
+                  this.af.database.object(`/userInfo/${user.auth.uid}`).update({
+                        name: user.auth.displayName,
+                        email: user.auth.email
+                    });
+                }
+               this.userCartList = this._userService.getUserCartInfo(user.auth.uid);
+               this.cartHomeList = this._mycartService.getUserCartHomeList(user.auth.uid);
+               this.cartHomeList.subscribe(list =>{
+                  //console.log(list);
+                 this.totalPriceAtCart = this._mycartService.getTotalCartPrice();
+               });
+      }
+      else {
+        console.log("User Not Signed IN");
+        this.cartHomeList = this._mycartService.getUserCartHomeList(null);
+        this.cartHomeList.subscribe(list =>{
+                  //console.log(list);
+                 this.totalPriceAtCart = this._mycartService.getTotalCartPrice();
+               });
+      }
+
     });
-    //this.cartHomeList = this._mycartService.getUserCartHomeList();
-    //console.log(this.cartHomeList);
   }
 
 }
